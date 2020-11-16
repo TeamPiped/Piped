782dbed016c5bc8d598fcf5ab82f89056236c592
FireMasterK <20838718+FireMasterK@users.noreply.github.com>
2020-11-16 16:06:37 +0530

    Automatically update service worker.

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -3,30 +3,34 @@
 import { register } from 'register-service-worker'
 
 if (process.env.NODE_ENV === 'production') {
-  register(`${process.env.BASE_URL}service-worker.js`, {
-    ready () {
-      console.log(
-        'App is being served from cache by a service worker.\n' +
-        'For more details, visit https://goo.gl/AFskqB'
-      )
-    },
-    registered () {
-      console.log('Service worker has been registered.')
-    },
-    cached () {
-      console.log('Content has been cached for offline use.')
-    },
-    updatefound () {
-      console.log('New content is downloading.')
-    },
-    updated () {
-      console.log('New content is available; please refresh.')
-    },
-    offline () {
-      console.log('No internet connection found. App is running in offline mode.')
-    },
-    error (error) {
-      console.error('Error during service worker registration:', error)
-    }
-  })
+    register(`${process.env.BASE_URL}service-worker.js`, {
+        ready() {
+            console.log(
+                'App is being served from cache by a service worker.\n' +
+                'For more details, visit https://goo.gl/AFskqB'
+            )
+        },
+        registered() {
+            console.log('Service worker has been registered.')
+        },
+        cached() {
+            console.log('Content has been cached for offline use.')
+        },
+        updatefound() {
+            console.log('New content is downloading.')
+        },
+        updated() {
+            console.log('New content is available; please refresh.')
+            caches.keys().then(function(names) {
+                for (let name of names) caches.delete(name);
+            });
+            window.location.reload()
+        },
+        offline() {
+            console.log('No internet connection found. App is running in offline mode.')
+        },
+        error(error) {
+            console.error('Error during service worker registration:', error)
+        }
+    })
 }
