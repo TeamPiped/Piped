b8dd41ba847d81595d3af497ef6c6d72d99463c1
FireMasterK <20838718+FireMasterK@users.noreply.github.com>
2020-11-16 23:39:09 +0530

    Fix container not occupying the full page.

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,21 @@
 <template>
-<nav class="uk-navbar-container uk-container-expand uk-light" style="background:#0b0e0f" uk-navbar>
-    <div class="uk-navbar-left">
-        <router-link class="uk-navbar-item uk-logo uk-text-bold" to="/">Piped</router-link>
-    </div>
-    <div class="uk-navbar-right">
-        <ul class="uk-navbar-nav">
-            <li>
-                <router-link to="/login">Login</router-link>
-            </li>
-            <li>
-                <router-link to="/feed">Feed</router-link>
-            </li>
-        </ul>
-    </div>
-</nav>
+<div class="uk-container uk-container-expand uk-light uk-height-viewport" style="background:#0b0e0f">
+    <nav class="uk-navbar-container uk-container-expand uk-light" style="background:#0b0e0f" uk-navbar>
+        <div class="uk-navbar-left">
+            <router-link class="uk-navbar-item uk-logo uk-text-bold" to="/">Piped</router-link>
+        </div>
+        <div class="uk-navbar-right">
+            <ul class="uk-navbar-nav">
+                <li>
+                    <router-link to="/login">Login</router-link>
+                </li>
+                <li>
+                    <router-link to="/feed">Feed</router-link>
+                </li>
+            </ul>
+        </div>
+    </nav>
 
-<div class="uk-container uk-container-expand uk-light" style="background:#0b0e0f">
     <router-view />
 </div>
 </template>
