09e5a9ecfa17fb6c443336ca90c650291f7bfe4c
FireMasterK <20838718+FireMasterK@users.noreply.github.com>
2021-01-02 02:31:44 +0530

    sw: don't remove cache

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -21,9 +21,6 @@ if (process.env.NODE_ENV === 'production') {
         },
         updated() {
             console.log('New content is available; please refresh.')
-            caches.keys().then(function (names) {
-                for (let name of names) caches.delete(name);
-            })
         },
         offline() {
             console.log('No internet connection found. App is running in offline mode.')
