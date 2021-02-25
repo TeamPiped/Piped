e9cfbb714b591cefd584e88d6a89d880d7492349
FireMasterK <20838718+FireMasterK@users.noreply.github.com>
2021-02-25 14:28:59 +0530

    Auto reload.

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -21,6 +21,7 @@ if (process.env.NODE_ENV === 'production') {
         },
         updated() {
             console.log('New content is available; please refresh.')
+            window.location.reload()
         },
         offline() {
             console.log('No internet connection found. App is running in offline mode.')
