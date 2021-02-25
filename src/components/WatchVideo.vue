2baef230c79d8704a100c25a803f7998a3267628
FireMasterK <20838718+FireMasterK@users.noreply.github.com>
2021-02-25 19:36:19 +0530

    Fix player destroyed incorrectly.

--- src/components/WatchVideo.vue
+++ src/components/WatchVideo.vue
@@ -105,7 +105,7 @@ export default {
     },
     beforeUnmount() {
         if (this.player) {
-            document.querySelector("video").remove();
+            window.player.destroy();
             window.player = undefined;
             window.ui = undefined;
         }
