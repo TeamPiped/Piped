2d29ce5972a6d48a280e433bb4169058f1f02e69
FireMasterK <20838718+FireMasterK@users.noreply.github.com>
2021-03-20 12:13:34 +0530

    Remove connection to google's servers for workbox.

--- vue.config.js
+++ vue.config.js
@@ -8,7 +8,8 @@ module.exports = {
         workboxPluginMode: 'GenerateSW',
         workboxOptions: {
             navigateFallback: 'index.html',
-            skipWaiting: true
+            skipWaiting: true,
+			importWorkboxFrom: 'local'
         }
     }
 }
