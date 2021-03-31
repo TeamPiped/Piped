d4ed70b6bbec8e051d7c633d2b1259204c66e3c7
Maurice Oegerli <maurice@oeger.li>
2021-03-31 21:14:21 +0200

    Add searchsuggestions component

---
+++ src/components/SearchSuggestions.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="uk-container-expand"></div>
+</template>
+
+<script>
+export default {
+    props: {
+        searchSuggestions: Array
+    }
+};
+</script>
+
+<style></style>
