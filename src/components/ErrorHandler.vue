20ad39b32714cbf011c78b41bda48436cf5abf72
FireMasterK <20838718+FireMasterK@users.noreply.github.com>
2021-06-07 22:55:16 +0530

    Wrap description text.
    And remove v-html for channels.

--- src/components/ErrorHandler.vue
+++ src/components/ErrorHandler.vue
@@ -3,7 +3,7 @@
     <button uk-toggle="target: #stacktrace" class="uk-button uk-button-small" style="background: #222" type="button">
         Show More
     </button>
-    <p id="stacktrace" style="white-space: pre" hidden>{{ error }}</p>
+    <p id="stacktrace" style="white-space: pre-wrap" hidden>{{ error }}</p>
 </template>
 
 <script>
