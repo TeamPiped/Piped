bae1cd3ce148003d6e9c67912410fc624a5723a7
FireMasterK <20838718+FireMasterK@users.noreply.github.com>
2021-07-20 02:55:43 +0530

    Add support for youtu.be 11 character redirects.

---
+++ src/components/VideoRedirect.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>Loading...</div>
+</template>
+
+
+<script>
+export default {
+    activated() {
+        const videoId = this.$route.params.videoId;
+        if (videoId)
+            this.$router.push({
+                path: "/watch",
+                query: { v: videoId },
+            });
+    },
+};
+</script>
