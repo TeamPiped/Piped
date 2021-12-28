98f1b84b6016959c8edeba8fcd21a65ff854be66
FireMasterK <20838718+FireMasterK@users.noreply.github.com>
2021-12-28 19:39:02 +0000

    Improve PWA support.

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,34 @@ export default defineConfig({
         vueI18n({
             include: path.resolve(__dirname, "./src/locales/**"),
         }),
-        VitePWA({ registerType: "autoUpdate" }),
+        VitePWA({
+            registerType: "autoUpdate",
+            workbox: {
+                globPatterns: ["**/*.{js,css,html,ico,svg,png}", "manifest.webmanifest"],
+            },
+            manifest: {
+                name: "Piped",
+                short_name: "Piped",
+                background_color: "#000000",
+                theme_color: "#fa4b4b",
+                icons: [
+                    { src: "./img/icons/android-chrome-192x192.png", sizes: "192x192", type: "image/png" },
+                    { src: "./img/icons/android-chrome-512x512.png", sizes: "512x512", type: "image/png" },
+                    {
+                        src: "./img/icons/android-chrome-maskable-192x192.png",
+                        sizes: "192x192",
+                        type: "image/png",
+                        purpose: "maskable",
+                    },
+                    {
+                        src: "./img/icons/android-chrome-maskable-512x512.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                        purpose: "maskable",
+                    },
+                ],
+            },
+        }),
     ],
     resolve: {
         alias: {
