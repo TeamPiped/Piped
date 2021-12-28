179dcbb687ae5373a8f0483cbb06bd9c208ca53d
FireMasterK <20838718+FireMasterK@users.noreply.github.com>
2021-12-28 14:59:58 +0000

    Fix PWA support.

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -1,33 +1,7 @@
 /* eslint-disable no-console */
 
-import { register } from "register-service-worker";
+import { registerSW } from "virtual:pwa-register";
 
 if (process.env.NODE_ENV === "production") {
-    register(`/service-worker.js`, {
-        ready() {
-            console.log(
-                "App is being served from cache by a service worker.\n" +
-                    "For more details, visit https://goo.gl/AFskqB",
-            );
-        },
-        registered() {
-            console.log("Service worker has been registered.");
-        },
-        cached() {
-            console.log("Content has been cached for offline use.");
-        },
-        updatefound() {
-            console.log("New content is downloading.");
-        },
-        updated() {
-            console.log("New content is available; please refresh.");
-            window.location.reload();
-        },
-        offline() {
-            console.log("No internet connection found. App is running in offline mode.");
-        },
-        error(error) {
-            console.error("Error during service worker registration:", error);
-        },
-    });
+    registerSW();
 }
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import WindiCSS from "vite-plugin-windicss";
 import vueI18n from "@intlify/vite-plugin-vue-i18n";
+import { VitePWA } from "vite-plugin-pwa";
 import path from "path";
 
 // https://vitejs.dev/config/
@@ -12,6 +13,7 @@ export default defineConfig({
         vueI18n({
             include: path.resolve(__dirname, "./src/locales/**"),
         }),
+        VitePWA({ registerType: "autoUpdate" }),
     ],
     resolve: {
         alias: {
