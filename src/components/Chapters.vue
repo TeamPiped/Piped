dfed90c1cca0ac162b65c5bff660e787d5b9ef5f
FireMasterK <20838718+FireMasterK@users.noreply.github.com>
2022-01-26 04:39:36 +0000

    Add max width to chapter.

--- src/components/Chapters.vue
+++ src/components/Chapters.vue
@@ -1,11 +1,6 @@
 <template>
     <h2 v-t="'video.chapters'" />
-    <div
-        :key="chapter.start"
-        v-for="chapter in props.chapters"
-        @click="$emit('seek', chapter.start)"
-        class="flex items-center mb-2 cursor-pointer w-sm"
-    >
+    <div :key="chapter.start" v-for="chapter in props.chapters" @click="$emit('seek', chapter.start)" class="chapter">
         <img :src="chapter.image" :alt="chapter.title" class="h-12" />
         <div class="ml-1">
             <span class="text-xl" v-text="chapter.title" />
@@ -14,6 +9,12 @@
         </div>
     </div>
 </template>
+
+<style>
+.chapter {
+    @apply flex items-center mb-2 cursor-pointer w-sm max-w-90vw;
+}
+</style>
 
 <script setup>
 import { defineProps, defineEmits } from "vue";
