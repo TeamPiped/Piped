ecd5d54aa20bf4a487812025b17d6f4b048917f6
FireMasterK <20838718+FireMasterK@users.noreply.github.com>
2022-02-14 06:56:03 +0000

    Do not define non-reactive data variables.
    Closes #829

--- src/components/VideoPlayer.vue
+++ src/components/VideoPlayer.vue
@@ -31,8 +31,6 @@ export default {
     },
     data() {
         return {
-            $player: null,
-            $ui: null,
             lastUpdate: new Date().getTime(),
             initialSeekComplete: false,
         };
