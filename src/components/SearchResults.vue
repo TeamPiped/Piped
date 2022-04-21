479cc40a45f7134718591f8087d38cf9e2898909
Palash <polashdeb@gmail.com>
2022-04-22 00:34:20 +0530

    filter button went full screen fixed

--- src/components/SearchResults.vue
+++ src/components/SearchResults.vue
@@ -11,7 +11,7 @@
         v-model="selectedFilter"
         default="all"
         class="uk-select uk-width-auto"
-        style="height: 100%"
+        style="height: auto"
         @change="updateResults()"
     >
         <option v-for="filter in availableFilters" :key="filter" :value="filter">
