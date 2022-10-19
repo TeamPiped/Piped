aaf1ca48b2d460b4c294833789a7b9ee190119a2
Kavin <20838718+FireMasterK@users.noreply.github.com>
2022-10-20 00:32:49 +0100

    Workaround emitter issue until upstream vite has a fix.
    See https://github.com/vitejs/vite/issues/10542

--- vite.config.js
+++ vite.config.js
@@ -68,6 +68,7 @@ export default defineConfig({
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
+            emitter: "node_modules/emitter-component/index.js",
         },
     },
     build: {
