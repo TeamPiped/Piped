756471695d13c332e58bf543b777c409e33f5d48
Kavin <20838718+FireMasterK@users.noreply.github.com>
2022-11-06 00:44:14 +0000

    Move v-if calls to single template.

--- src/components/WatchOnYouTubeButton.vue
+++ src/components/WatchOnYouTubeButton.vue
@@ -7,14 +7,16 @@ export default {
 </script>
 
 <template>
-    <!-- For large screens -->
-    <a :href="link" v-if="this.getPreferenceBoolean('showWatchOnYouTube', false)" class="btn lt-lg:hidden">
-        <i18n-t keypath="player.watch_on" tag="strong">
+    <template v-if="this.getPreferenceBoolean('showWatchOnYouTube', false)">
+        <!-- For large screens -->
+        <a :href="link" class="btn lt-lg:hidden">
+            <i18n-t keypath="player.watch_on" tag="strong">
+                <font-awesome-icon class="mx-1.5" :icon="['fab', 'youtube']" />
+            </i18n-t>
+        </a>
+        <!-- For small screens -->
+        <a :href="link" class="btn lg:hidden">
             <font-awesome-icon class="mx-1.5" :icon="['fab', 'youtube']" />
-        </i18n-t>
-    </a>
-    <!-- For small screens -->
-    <a :href="link" v-if="this.getPreferenceBoolean('showWatchOnYouTube', false)" class="btn lg:hidden">
-        <font-awesome-icon class="mx-1.5" :icon="['fab', 'youtube']" />
-    </a>
+        </a>
+    </template>
 </template>
