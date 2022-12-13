585605312f43b6aa10ea092af9b599e045cb4ae5
PrivacyDev <privacydev@danwin1210.de>
2022-12-12 19:15:44 -0500

    fixed bug in localizefonts.sh

--- localizefonts.sh
+++ localizefonts.sh
@@ -3,8 +3,8 @@
 base='https://fonts\.(gstatic\.com|kavin\.rocks)'
 fonts=$(cat dist/assets/* | grep -Po "$base[^)]*" | sort | uniq)
 for font in $fonts; do
-	file="dist/fonts$(echo $font | sed "s#$base##")"
+	file="dist/fonts$(echo $font | sed -E "s#$base##")"
 	mkdir -p "$(dirname "$file")"
 	curl -L "$font" -o "$file"
 done
-sed -i "s#$base#/fonts#g" dist/assets/*
+sed -Ei "s#$base#/fonts#g" dist/assets/*
