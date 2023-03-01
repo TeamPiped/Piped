c6e1ab848c87413a4c3c66307ebe808758ff30c2
vr10t <ai6@tuta.io>
2023-03-01 18:00:18 +0000

    add pagination to history page

--- src/components/HistoryPage.vue
+++ src/components/HistoryPage.vue
@@ -33,6 +33,9 @@ export default {
     },
     data() {
         return {
+            currentVideoCount: 0,
+            videoStep: 100,
+            videosStore: [],
             videos: [],
         };
     },
@@ -42,29 +45,39 @@ export default {
                 var tx = window.db.transaction("watch_history", "readonly");
                 var store = tx.objectStore("watch_history");
                 const cursorRequest = store.index("watchedAt").openCursor(null, "prev");
-                cursorRequest.onsuccess = e => {
-                    const cursor = e.target.result;
-                    if (cursor) {
-                        const video = cursor.value;
-                        this.videos.push({
-                            url: "/watch?v=" + video.videoId,
-                            title: video.title,
-                            uploaderName: video.uploaderName,
-                            uploaderUrl: video.uploaderUrl,
-                            duration: video.duration,
-                            thumbnail: video.thumbnail,
-                            watchedAt: video.watchedAt,
-                            watched: true,
-                            currentTime: video.currentTime,
-                        });
-                        if (this.videos.length < 1000) cursor.continue();
-                    }
-                };
+                const cursorPromise = new Promise(resolve => {
+                    cursorRequest.onsuccess = e => {
+                        const cursor = e.target.result;
+                        if (cursor) {
+                            const video = cursor.value;
+                            this.videosStore.push({
+                                url: "/watch?v=" + video.videoId,
+                                title: video.title,
+                                uploaderName: video.uploaderName,
+                                uploaderUrl: video.uploaderUrl,
+                                duration: video.duration,
+                                thumbnail: video.thumbnail,
+                                watchedAt: video.watchedAt,
+                                watched: true,
+                                currentTime: video.currentTime,
+                            });
+                            if (this.videosStore.length < 1000) cursor.continue();
+                            else resolve();
+                        } else resolve();
+                    };
+                });
+                await cursorPromise;
             }
-        })();
+        })().then(() => {
+            this.loadMoreVideos();
+        });
     },
     activated() {
         document.title = "Watch History - Piped";
+        window.addEventListener("scroll", this.handleScroll);
+    },
+    deactivated() {
+        window.removeEventListener("scroll", this.handleScroll);
     },
     methods: {
         clearHistory() {
@@ -90,6 +103,17 @@ export default {
                 ],
             };
             this.download(JSON.stringify(json), `piped_history_${dateStr}.json`, "application/json");
+        },
+        loadMoreVideos() {
+            console.log("load more videos", this.videosStore.length, this.currentVideoCount);
+            this.currentVideoCount = Math.min(this.currentVideoCount + this.videoStep, this.videosStore.length);
+            if (this.videos.length != this.videosStore.length)
+                this.videos = this.videosStore.slice(0, this.currentVideoCount);
+        },
+        handleScroll() {
+            if (window.innerHeight + window.scrollY >= document.body.offsetHeight - window.innerHeight) {
+                this.loadMoreVideos();
+            }
         },
     },
 };
