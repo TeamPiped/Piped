288b4907d6a501a7c590d08082e0b8361828f099
Kavin <20838718+FireMasterK@users.noreply.github.com>
2023-03-21 16:22:13 +0000

    Fix for playlist uploaders/creators being unclickable.

--- src/components/PlaylistItem.vue
+++ src/components/PlaylistItem.vue
@@ -10,14 +10,15 @@
             </p>
         </router-link>
         <p v-if="props.item.description" v-text="props.item.description" />
+
         <router-link v-if="props.item.uploaderUrl" class="link" :to="props.item.uploaderUrl">
             <p>
-                <span v-text="props.item.uploader" />
+                <span v-text="props.item.uploaderName" />
                 <font-awesome-icon class="ml-1.5" v-if="props.item.uploaderVerified" icon="check" />
             </p>
         </router-link>
+        <a v-else-if="props.item.uploaderName" class="link" v-text="props.item.uploaderName" />
 
-        <a v-if="props.item.uploaderName" class="link" v-text="props.item.uploaderName" />
         <template v-if="props.item.videos >= 0">
             <br v-if="props.item.uploaderName" />
             <strong v-text="`${props.item.videos} ${$t('video.videos')}`" />
