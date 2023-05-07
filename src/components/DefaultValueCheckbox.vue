c217d5e4e3ed25a91a0c88a8a345a25b4be5a265
Bnyro <bnyro@tutanota.com>
2023-05-07 19:56:56 +0200

    Subscription groups

---
+++ src/components/DefaultValueCheckbox.vue
@@ -0,0 +1,22 @@
+// Wrapper around v-model to allow default values without requiring to use a v-model inside the calling component
+
+<script>
+export default {
+    props: {
+        defaultValue: Boolean,
+        callback: Function,
+    },
+    data() {
+        return {
+            value: false,
+        };
+    },
+    mounted() {
+        this.value = this.defaultValue;
+    },
+};
+</script>
+
+<template>
+    <input type="checkbox" class="checkbox" v-model="value" @change="callback()" />
+</template>
