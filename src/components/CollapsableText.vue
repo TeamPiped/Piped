8b4949349ea9788259f5c75dab59b4dbb9d28205
Bnyro <bnyro@tutanota.com>
2023-12-19 16:44:16 +0100

    feat: option to create group in add to group modal

--- src/components/CollapsableText.vue
+++ src/components/CollapsableText.vue
@@ -7,7 +7,7 @@
         <span v-if="text.length > visibleLimit && !showFullText">...</span>
         <button
             v-if="text.length > visibleLimit"
-            class="block whitespace-normal font-semibold text-neutral-500 hover:underline"
+            class="block whitespace-normal text-neutral-500 font-semibold hover:underline"
             @click="showFullText = !showFullText"
         >
             [{{ showFullText ? $t("actions.show_less") : $t("actions.show_more") }}]
