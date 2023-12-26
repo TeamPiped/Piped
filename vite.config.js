c3bc24ea625c1e4f3180d194e20e2089f00659bf
fk <2378306+fkrull@users.noreply.github.com>
2023-12-26 21:12:17 +0100

    Set vite base URL from env var

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ import eslintPlugin from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: process.env.VITE_BASE_URL,
     plugins: [
         vue(),
         Unocss(),
