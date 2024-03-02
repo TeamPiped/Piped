3e7359f9cf4adbaa2fbbced620ec02530c2c9806
fk <2378306+fkrull@users.noreply.github.com>
2024-03-02 21:49:24 +0100

    Don't set vite base path from env

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,6 @@ import eslintPlugin from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: process.env.VITE_BASE_URL,
     plugins: [
         vue(),
         Unocss(),
