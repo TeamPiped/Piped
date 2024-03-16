256cab83a0006c9ba685a08c74634df5b15ebb58
Andy Russo <88431411+AndyRusso@users.noreply.github.com>
2024-03-16 21:02:19 +0500

    feat: auto focus CreatePlaylistModal input field

--- src/components/CreatePlaylistModal.vue
+++ src/components/CreatePlaylistModal.vue
@@ -2,7 +2,7 @@
     <ModalComponent @close="$emit('close')">
         <div class="flex flex-col">
             <h2 v-t="'actions.create_playlist'" />
-            <input v-model="playlistName" type="text" class="input mt-2" />
+            <input ref="input" v-model="playlistName" type="text" class="input mt-2" />
             <div class="ml-auto mt-3 w-min flex">
                 <button v-t="'actions.cancel'" class="btn" @click="$emit('close')" />
                 <button v-t="'actions.okay'" class="btn ml-2" @click="onCreatePlaylist" />
@@ -25,6 +25,7 @@ export default {
         };
     },
     mounted() {
+        this.$refs.input.focus();
         window.addEventListener("keydown", this.handleKeyDown);
     },
     unmounted() {
