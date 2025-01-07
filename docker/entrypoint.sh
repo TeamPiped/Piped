43cca22d41b26e3339997ac293f07d9ce02d79fc
Bnyro <bnyro@tutanota.com>
2025-01-07 20:56:44 +0100

    docker: allow setting frontend http mode (http / https)

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,5 +5,8 @@ if [ -z "$BACKEND_HOSTNAME" ]; then
     exit 1
 fi
 
+HTTP_MODE=${HTTP_MODE:-https}
+
+sed -i s@https://pipedapi.kavin.rocks@"$HTTP_MODE"://pipedapi.kavin.rocks@g /usr/share/nginx/html/assets/*
 sed -i s/pipedapi.kavin.rocks/"$BACKEND_HOSTNAME"/g /usr/share/nginx/html/assets/*
 nginx -g "daemon off;"
